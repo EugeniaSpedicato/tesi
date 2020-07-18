be457592d64fc328ae0ac29a4a9642c5787484ad
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-07-18 11:54:56 +0200

    jpg

--- atree.C
+++ atree.C
@@ -1,11 +1,14 @@
 #define atree_cxx
 #include "atree.h"
 #include <TH2.h>
+#include <TH1.h>
+
 #include <TStyle.h>
 #include <TCanvas.h>
 
 void atree::Loop()
 {
+TH1::SetDefaultSumw2();
 TH1F* E_mu=new TH1F("h1", "Energy muon", 150,0,200);
 TH1F* E_e=new TH1F("h2", "Energy electron", 150,0,200);
 
@@ -67,66 +70,66 @@ TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",140,-0.3,-0.3,100,
       // if (Cut(ientry) < 0) continue;
        Double_t pmuOUT=sqrt(detKinBeamRot_pXmu_out*detKinBeamRot_pXmu_out+detKinBeamRot_pYmu_out*detKinBeamRot_pYmu_out+detKinBeamRot_pZmu_out*detKinBeamRot_pZmu_out);
        Double_t peOUT=sqrt(detKinBeamRot_pXe_out*detKinBeamRot_pXe_out+detKinBeamRot_pYe_out*detKinBeamRot_pYe_out+detKinBeamRot_pZe_out*detKinBeamRot_pZe_out);
-       Double_t E_mu=sqrt(pmuOUT*pmuOUT+(105.6583745 *0.001)*(105.6583745 *0.001));
-       Double_t E_e=sqrt(peOUT*peOUT+(0.5109989461 *0.001)*(0.5109989461 *0.001));
+       Double_t DE_mu=sqrt(pmuOUT*pmuOUT+(105.6583745 *0.001)*(105.6583745 *0.001));
+       Double_t DE_e=sqrt(peOUT*peOUT+(0.5109989461 *0.001)*(0.5109989461 *0.001));
        
-       E_mu->Fill(E_mu);
-       E_e->Fill(E_e);
+       E_mu->Fill(DE_mu,wgt_full);
+       E_e->Fill(DE_e,wgt_full);
     
        
-       px_mu->Fill(detKinBeamRot_pXmu);
-       py_mu->Fill(detKinBeamRot_pYmu);
-       pz_mu->Fill(detKinBeamRot_pZmu);
+       px_mu->Fill(detKinBeamRot_pXmu,wgt_full);
+       py_mu->Fill(detKinBeamRot_pYmu,wgt_full);
+       pz_mu->Fill(detKinBeamRot_pZmu,wgt_full);
        
-       px_mu_out->Fill(detKinBeamRot_pXmu_out);
-       py_mu_out->Fill(detKinBeamRot_pYmu_out);
-       pz_mu_out->Fill(detKinBeamRot_pZmu_out);
-       px_mu_outNO->Fill(detKin_pXmu_out);
-       py_mu_outNO->Fill(detKin_pYmu_out);
-       pz_mu_outNO->Fill(detKin_pZmu_out);
+       px_mu_out->Fill(detKinBeamRot_pXmu_out,wgt_full);
+       py_mu_out->Fill(detKinBeamRot_pYmu_out,wgt_full);
+       pz_mu_out->Fill(detKinBeamRot_pZmu_out,wgt_full);
+       px_mu_outNO->Fill(detKin_pXmu_out,wgt_full);
+       py_mu_outNO->Fill(detKin_pYmu_out,wgt_full);
+       pz_mu_outNO->Fill(detKin_pZmu_out,wgt_full);
        
-       px_e_out->Fill(detKinBeamRot_pXe_out);
-       py_e_out->Fill(detKinBeamRot_pYe_out);
-       pz_e_out->Fill(detKinBeamRot_pZe_out);
-       px_e_outNO->Fill(detKin_pXe_out);
-       py_e_outNO->Fill(detKin_pYe_out);
-       pz_e_outNO->Fill(detKin_pZe_out);
+       px_e_out->Fill(detKinBeamRot_pXe_out,wgt_full);
+       py_e_out->Fill(detKinBeamRot_pYe_out,wgt_full);
+       pz_e_out->Fill(detKinBeamRot_pZe_out,wgt_full);
+       px_e_outNO->Fill(detKin_pXe_out,wgt_full);
+       py_e_outNO->Fill(detKin_pYe_out,wgt_full);
+       pz_e_outNO->Fill(detKin_pZe_out,wgt_full);
        
-        coox_mu->Fill(detKinBeamRot_cooXmu);
-       cooy_mu->Fill(detKinBeamRot_cooYmu);
+        coox_mu->Fill(detKinBeamRot_cooXmu,wgt_full);
+       cooy_mu->Fill(detKinBeamRot_cooYmu,wgt_full);
        
-       coox_e->Fill(detKinBeamRot_cooXe);
-       cooy_e->Fill(detKinBeamRot_cooYe);
+       coox_e->Fill(detKinBeamRot_cooXe,wgt_full);
+       cooy_e->Fill(detKinBeamRot_cooYe,wgt_full);
        
        Double_t diffX=detKinBeamRot_cooXe-detKinBeamRot_cooXmu;
-       diffX_mue->Fill(diffX);
+       diffX_mue->Fill(diffX,wgt_full);
        Double_t diffY=detKinBeamRot_cooYe-detKinBeamRot_cooYmu;
-       diffY_mue->Fill(diffY);
+       diffY_mue->Fill(diffY,wgt_full);
        
        Double_t diffPX=detKinBeamRot_pXe_out-detKin_pXe_out;
-       diffePX->Fill(diffPX);
+       diffePX->Fill(diffPX,wgt_full);
        Double_t diffPY=detKinBeamRot_pYe_out-detKin_pYe_out;
-       diffePY->Fill(diffPY);
+       diffePY->Fill(diffPY,wgt_full);
        Double_t diffPZ=detKinBeamRot_pZe_out-detKin_pZe_out;
-       diffePZ->Fill(diffPZ);
+       diffePZ->Fill(diffPZ,wgt_full);
        
-     X_Y_mu ->Fill(detKinBeamRot_cooXmu, detKinBeamRot_cooYmu);
-     X_Y_e ->Fill(detKinBeamRot_cooXe, detKinBeamRot_cooYe);
+     X_Y_mu ->Fill(detKinBeamRot_cooXmu, detKinBeamRot_cooYmu,wgt_full);
+     X_Y_e ->Fill(detKinBeamRot_cooXe, detKinBeamRot_cooYe,wgt_full);
        
-       if (tar==0)
+       if (detKinBeamRot_tar==0)
        {
-         tarONEXmu->Fill(detKinBeamRot_cooXmu);
-         tarONEYmu->Fill(detKinBeamRot_cooYmu);
-         tarONEXe->Fill(detKinBeamRot_cooXe);
-         tarONEYe->Fill(detKinBeamRot_cooYe);
+         tarONEXmu->Fill(detKinBeamRot_cooXmu,wgt_full);
+         tarONEYmu->Fill(detKinBeamRot_cooYmu,wgt_full);
+         tarONEXe->Fill(detKinBeamRot_cooXe,wgt_full);
+         tarONEYe->Fill(detKinBeamRot_cooYe,wgt_full);
        }
        
-       if (tar==1)
+       if (detKinBeamRot_tar==1)
        {
-         tarTWOXmu->Fill(detKinBeamRot_cooXmu);
-         tarTWOYmu->Fill(detKinBeamRot_cooYmu);
-         tarTWOXe->Fill(detKinBeamRot_cooXe);
-         tarTWOYe->Fill(detKinBeamRot_cooYe);
+         tarTWOXmu->Fill(detKinBeamRot_cooXmu,wgt_full);
+         tarTWOYmu->Fill(detKinBeamRot_cooYmu,wgt_full);
+         tarTWOXe->Fill(detKinBeamRot_cooXe,wgt_full);
+         tarTWOYe->Fill(detKinBeamRot_cooYe,wgt_full);
            
        }
        
@@ -135,54 +138,62 @@ TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",140,-0.3,-0.3,100,
     px_mu->Fit("gaus");
     py_mu->Fit("gaus");
     pz_mu->Fit("gaus");
-    /*
+    
     TCanvas * diffP= new TCanvas("diff","diff",400,10,1500,1000);
     diffP->Divide(3,2);
     diffP->cd(1);
     px_e_outNO->SetLineColor(kOrange);
-    px_e_outNO->Draw();
+    px_e_outNO->Draw("L");
     px_e_out->SetLineColor(kBlack);
-    px_e_out->Draw("same");
+    px_e_out->Draw("SAME");
+    px_e_out->Draw("L");
+    
     
     
     diffP->cd(2);
     py_e_outNO->SetLineColor(kOrange);
-    py_e_outNO->Draw();
+    py_e_outNO->Draw("L");
     py_e_out->SetLineColor(kBlack);
     py_e_out->Draw("same");
+    py_e_out->Draw("L");
+    
     
     
     
     diffP->cd(3);
     pz_e_outNO->SetLineColor(kOrange);
-    pz_e_outNO->Draw();
+    pz_e_outNO->Draw("L");
     pz_e_out->SetLineColor(kBlack);
     pz_e_out->Draw("same");
+    pz_e_out->Draw("L");
+    
     
     
     diffP->cd(4);
     px_mu_outNO->SetLineColor(40);
-    px_mu_outNO->Draw();
+    px_mu_outNO->Draw("L");
     px_mu_out->SetLineColor(46);
     px_mu_out->Draw("same");
+    px_mu_out->Draw("L");
+    
     
     diffP->cd(5);
     px_mu_outNO->SetLineColor(40);
-    px_mu_outNO->Draw();
+    px_mu_outNO->Draw("L");
     px_mu_out->SetLineColor(46);
     px_mu_out->Draw("same");
+    px_mu_out->Draw("L");
+    
     
     diffP->cd(6);
     px_mu_outNO->SetLineColor(40);
-    px_mu_outNO->Draw();
+    px_mu_outNO->Draw("L");
     px_mu_out->SetLineColor(46);
     px_mu_out->Draw("same");
+    px_mu_out->Draw("L");
     
     
-    
-    
-    
-    diffP->SaveAs("divNodiv.png");
+    diffP->SaveAs("divNodiv.jpg");
     
         
         
@@ -190,12 +201,12 @@ TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",140,-0.3,-0.3,100,
      TCanvas * d= new TCanvas("d","d",400,10,1500,1000);
     d->Divide(3,1);
     d->cd(1);
-    diffePX->Draw();
+    diffePX->Draw("L");
     d->cd(2);
-    diffePY->Draw();
+    diffePY->Draw("L");
     d->cd(3);
-    diffePZ->Draw();
-    diffP->SaveAs("diffEp.png");
+    diffePZ->Draw("L");
+    d->SaveAs("diffEp.jpg");
     
     
     
@@ -203,69 +214,79 @@ TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",140,-0.3,-0.3,100,
     TCanvas * E= new TCanvas("E","E",400,10,1500,1000);
     E->Divide(2,1);
     E->cd(1);
-    E_mu->Draw();
+    E_mu->Draw("L");
     E->cd(2);
     E_e->SetLineColor(kRed);
-    E_e->Draw();
+    E_e->Draw("L");
     
     TCanvas * Pin= new TCanvas("Pin","Pin",400,10,1500,1000);
     Pin->Divide(1,3);
     Pin->cd(1);
-    px_mu->Draw();
+    px_mu->Draw("L");
     Pin->cd(2);
-    py_mu->Draw();
+    py_mu->Draw("L");
     Pin->cd(3);
-    pz_mu->Draw();
+    pz_mu->Draw("L");
         
     TCanvas * diff= new TCanvas("diff","diff",400,10,1500,1000);
     diff->Divide(2,2);
     diff->cd(1);
     px_e_out->SetLineColor(kRed);
-    px_e_out->Draw();
+    px_e_out->Draw("L");
     px_mu_out->Draw("same");
+    px_mu_out->Draw("L");
+    
     
     
     diff->cd(2);
     py_e_out->SetLineColor(kRed);
-    py_e_out->Draw();
+    py_e_out->Draw("L");
     py_mu_out->Draw("same");
+    py_mu_out->Draw("L");
+    
     
     
     diff->cd(3);
-    pz_mu_out->Draw();
+    pz_mu_out->Draw("L");
     
     diff->cd(4);
-    pz_e_out->Draw();
-     */
+    pz_e_out->Draw("L");
+     diff->SaveAs("diff.jpg");
     
     TCanvas * cooX= new TCanvas("cooX","cooX",400,10,600,400);
     cooX->Divide(2,2);
     cooX->cd(1);
-    coox_mu->Draw();
+    coox_mu->Draw("L");
     coox_e->SetLineColor(kRed);
     coox_e->Draw("same");
+    coox_e->Draw("L");
+    
 
  
     cooX->cd(2);
-    cooy_mu->Draw();
+    cooy_mu->Draw("L");
     cooy_e->SetLineColor(kRed);
     cooy_e->Draw("same");
+    cooy_e->Draw("L");
+    
  
     cooX->cd(3);
-    diffX_mue->Draw();
+    diffX_mue->Draw("L");
       
     cooX->cd(4);
     diffY_mue->SetLineColor(kRed);
-    diffY_mue->Draw();
-  cooX->SaveAs("coo.png");
+    diffY_mue->Draw("L");
+  cooX->SaveAs("coo.jpg");
  
     TCanvas * dued= new TCanvas("dued","dued",400,10,600,400);
-    tar->Divide(3,1);
+
   X_Y_mu->SetMarkerColor(kBlack);
-    X_Y_mu->Draw();
+    X_Y_mu->Draw("L");
   X_Y_e->SetMarkerColor(kRed);
     X_Y_e->Draw("same");
-  dued->SaveAs("duedcoo.png");
+    X_Y_e->Draw("L");
+    
+  dued->SaveAs("duedcoo.jpg");
     
     TCanvas * tar= new TCanvas("tar","tar",400,10,600,400);
     tar->Divide(2,1);
@@ -279,7 +300,7 @@ TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",140,-0.3,-0.3,100,
     tarTWOYmu->Draw();
       tarTWOYe->SetMarkerColor(49);
     tarTWOYe->Draw("same");
-  dued->SaveAs("duedcoo.png");
+  tar->SaveAs("tar.jpg");
 
     
 }

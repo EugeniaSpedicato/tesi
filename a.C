6ef7e0669eb394f9bdc431315d9cb0b4bd452749
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-07-20 16:46:00 +0200

    ??

---
+++ a.C
@@ -0,0 +1,31 @@
+#define atree_cxx
+#include "atree.h"
+#include <TH2.h>
+#include <TH1.h>
+
+#include <TStyle.h>
+#include <TCanvas.h>
+
+void atree::Loop()
+{
+    TH1::SetDefaultSumw2();
+    
+    
+     if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+       
+       
+   }
+    
+    
+    
+    
+    
+}

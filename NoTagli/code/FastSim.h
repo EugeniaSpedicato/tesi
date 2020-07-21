e57e81ff1a6b6ed353acc333534c5d9a46538a02
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-07-21 10:54:23 +0200

    FastSi

--- NoTagli/code/FastSim.h
+++ NoTagli/code/FastSim.h
@@ -55,7 +55,7 @@ namespace MuE {
     void LoadKineVars(const PxPyPzEVector & p_mu_in,  const PxPyPzEVector & p_e_in, 
 		      const PxPyPzEVector & p_mu_out, const PxPyPzEVector & p_e_out, 
 		      KineVars & kv);
-    void LoadPhoton(const Event & event, Photon & photon, const PxPyPzEVector & p_mu_in);
+    void LoadPhoton(const Event & event, Photon & photon);
 
     static const Double_t mm_PDG; // PDG muon mass 
     static const Double_t me_PDG; // PDG electron mass

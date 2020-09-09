82db6cba9feccf8ffbaaf2e73db0b082c6409d0c
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-09-09 11:44:11 +0200

    from tar 0 con restrizioni

--- next.C
+++ next.C
@@ -87,7 +87,7 @@ TH2F  *X_Y_p  = new TH2F("h2da" , " X  Vs. y of the photon",140,-0.5,-0.5,140,-0
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
  
-       if (tar==0){
+       if (detKinBeamRot_tar==0){
        px_mu->Fill(detKinBeamRot_pXmu,wgt_full);
        py_mu->Fill(detKinBeamRot_pYmu,wgt_full);
        pz_mu->Fill(detKinBeamRot_pZmu,wgt_full);

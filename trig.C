6d294a6fa654b190c9c1b9775d518dcdba12c204
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-09-18 11:50:57 +0200

    prova retta

--- trig.C
+++ trig.C
@@ -128,7 +128,7 @@ TH2F  *Ee_Eph = new TH2F("h2da1" , " E e Vs. E oh of the photons with d<2Rm",140
    
        //energyThEl->SetPoint(jentry,detKinBeamRot_the,detKinBeamRot_Ee);
        
-    if ((detKinBeamRot_Ee>3.5 || detKinBeamRot_the>12) && (detKinBeamRot_Ee>40 || detKinBeamRot_the>4) && (detKinBeamRot_Ee>(-168/3.4)+((105/3.4)*detKinBeamRot_thmu)) && ( detKinBeamRot_Ee>125 || detKinBeamRot_Ee<25+(100/3.5)*detKinBeamRot_thmu )){   
+    if ((detKinBeamRot_Ee>3.5 || detKinBeamRot_the>12) && (detKinBeamRot_Ee>40 || detKinBeamRot_the>4) && (detKinBeamRot_Ee>(-168/3.4)+((105/3.4)*detKinBeamRot_thmu)) && ( detKinBeamRot_Ee>125 || detKinBeamRot_Ee<25+(100/3.5)*detKinBeamRot_thmu ) && ){   
        
        if (detKinBeamRot_cooXe < 0.07 && detKinBeamRot_cooYe < 0.07 && detKinBeamRot_cooXe > -0.07 && detKinBeamRot_cooYe > -0.07)
         {
@@ -276,11 +276,14 @@ d=sqrt((detKinBeamRot_cooXe-photon_coox)*(detKinBeamRot_cooXe-photon_coox)+(detK
     if (abs(detKinBeamRot_cooXe)<0.07 && abs(detKinBeamRot_cooYe)<0.07 && abs(detKinBeamRot_cooXmu)<0.07 && abs(detKinBeamRot_cooYmu)<0.07 && detKinBeamRot_tar==0)
        {
           Th_E_mu0->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
-           if (photon_coox != -1)
+        
+        d=sqrt((detKinBeamRot_cooXe-photon_coox)*(detKinBeamRot_cooXe-photon_coox)+(detKinBeamRot_cooYe-photon_cooy)*(detKinBeamRot_cooYe-photon_cooy)); 
+        
+           if (photon_coox != -1 && d<2*Rm)
            {
                Th_E_eph0->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
            }
-           else Th_E_eNoph0->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
+           else if (photon_coox = -1) {Th_E_eNoph0->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);}
        }
        
    
@@ -288,21 +291,21 @@ d=sqrt((detKinBeamRot_cooXe-photon_coox)*(detKinBeamRot_cooXe-photon_coox)+(detK
     if (abs(detKinBeamRot_cooXe)<0.07 && abs(detKinBeamRot_cooYe)<0.07 && abs(detKinBeamRot_cooXmu)<0.07 && abs(detKinBeamRot_cooYmu)<0.07 && detKinBeamRot_tar==1)
        {
           Th_E_mu1->Fill(detKinBeamRot_thmu,detKinBeamRot_Emu,wgt_full);
-           if (photon_coox != -1)
+           if (photon_coox != -1 && d<2*Rm)
            {
                Th_E_eph1->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
            }
-           else Th_E_eNoph1->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
+           else if (photon_coox = -1) { Th_E_eNoph1->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);}
        }
         
             if (abs(detKinBeamRot_cooXe)<0.07 && abs(detKinBeamRot_cooYe)<0.07 && abs(detKinBeamRot_cooXmu)<0.07 && abs(detKinBeamRot_cooYmu)<0.07)
        {
           Th_E_mu->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
-           if (photon_coox != -1)
+           if (photon_coox != -1 && d<2*Rm)
            {
                Th_E_eph->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
            }
-           else Th_E_eNoph->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);
+           else if (photon_coox = -1) { Th_E_eNoph->Fill(detKinBeamRot_the,detKinBeamRot_thmu,wgt_full);}
        }
        
    }

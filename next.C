05e09b9c9fdbe1aac0b088d24123eafa40425d3c
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-09-29 10:01:26 +0200

    colz

--- next.C
+++ next.C
@@ -73,9 +73,9 @@ TH1F* dymp=new TH1F("h2ea", "diff coo Y photon and mu ", 070,-0.1,0.1);
 TH1F* dxep=new TH1F("h2ea", "diff coo X e and photon", 070,-0.1,0.1);
 TH1F* dyep=new TH1F("h2ea", "diff coo Y e and photon ", 070,-0.1,0.1);
     
-TH2F  *X_Y_mu  = new TH2F("h2d" , " X  Vs. y of the muon",1000,-0.5,0.5,1000,-0.5,0.5);
-TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",1000,-0.5,0.5,1000,-0.5,0.5);
-TH2F  *X_Y_p  = new TH2F("h2da" , " X  Vs. y of the photon",1000,-0.5,0.5,1000,-0.5,0.5);
+TH2F  *X_Y_mu  = new TH2F("h2d" , " X  Vs. y of the muon",1000,-0.1,0.1,1000,-0.1,0.1);
+TH2F  *X_Y_e  = new TH2F("h2da" , " X  Vs. y of the electron",1000,-0.1,0.1,1000,-0.1,0.1);
+TH2F  *X_Y_p  = new TH2F("h2da" , " X  Vs. y of the photon",1000,-0.1,0.1,1000,-0.1,0.1);
  
     
      if (fChain == 0) return;
@@ -176,34 +176,30 @@ TH2F  *X_Y_p  = new TH2F("h2da" , " X  Vs. y of the photon",1000,-0.5,0.5,1000,-
         
        
        
-       if(abs(photon_coox) < 1 && abs(photon_cooy) < 1)
+       if(abs(photon_coox) < 0.07 && abs(photon_cooy) < 0.07)
        {
         Ep->Fill(photon_energy,wgt_full);            
                if (detKinBeamRot_tar==0)
        
                {
-             if (abs(photon_coox)<0.07 && abs(photon_cooy)<0.07)
-        {tarONEXp->Fill(photon_coox,wgt_full);
-         tarONEYp->Fill(photon_cooy,wgt_full);}
+            tarONEXp->Fill(photon_coox,wgt_full);
+         tarONEYp->Fill(photon_cooy,wgt_full);
                }
             
                if (detKinBeamRot_tar==1)
        
                {
-
-         if (abs(photon_coox)<0.07 && abs(photon_cooy)<0.07)
-        {tarTWOXp->Fill(photon_coox,wgt_full);
-         tarTWOYp->Fill(photon_cooy,wgt_full);}                  
-               }
-       
-     if (abs(photon_coox)<0.07 && abs(photon_cooy)<0.07)
-        {X_Y_p ->Fill(photon_coox, photon_cooy,wgt_full);}    
+         tarTWOXp->Fill(photon_coox,wgt_full);
+         tarTWOYp->Fill(photon_cooy,wgt_full);
+               }                  
+               
+           X_Y_p ->Fill(photon_coox, photon_cooy,wgt_full);}    
        }
 
 
 
        
-       if (abs(detKinBeamRot_cooXmu) <0.07 && abs(detKinBeamRot_cooYmu) <0.07 && abs(detKinBeamRot_cooXe) <0.07 && abs(detKinBeamRot_cooYe) <0.07)
+       if (abs(detKinBeamRot_cooXmu) <0.07 && abs(detKinBeamRot_cooYmu) <0.07 && abs(detKinBeamRot_cooXe) <0.07 && abs(detKinBeamRot_cooYe) <0.07 && abs(photon_coox) < 0.07 && abs(photon_cooy) < 0.07)
        {Double_t Dx = detKinBeamRot_cooXe-detKinBeamRot_cooXmu;
     Double_t Dy = detKinBeamRot_cooYe-detKinBeamRot_cooXmu;
     Double_t Dxep = detKinBeamRot_cooXe-photon_coox;

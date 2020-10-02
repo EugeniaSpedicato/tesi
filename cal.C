0f40a6e45605a400f8d8711955dac0ad9b0fe191
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-10-02 16:06:59 +0200

    mh

--- cal.C
+++ cal.C
@@ -109,10 +109,10 @@ if (abs(detKinBeamRot_cooXe)<7.125 && abs(detKinBeamRot_cooYe)<7.125)
        
        
        
-       //&& photon_energy>0.2
        
        
-if (abs(photon_coox)<7.125 && abs(photon_cooy)<7.125 )
+       
+if (abs(photon_coox)<7.125 && abs(photon_cooy)<7.125 && photon_energy>0.2)
     
 { n_tot++;
     if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=1;}

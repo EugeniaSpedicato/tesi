81d64c6b59185089e7567ef51e76bbe5de32f094
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-10-12 16:04:36 +0200

    ecal

--- ECAL.C
+++ ECAL.C
@@ -1,7 +1,6 @@
 #include <TH2.h>
 #include <TH1.h>
 #include <TGraph.h>
-#include <TTree.h>
 #include <cmath>
 #include <TMath.h>
 using namespace std;
@@ -11,23 +10,15 @@ using namespace std;
 
 void ECAL()
 {
-    TH1::SetDefaultSumw2();
     
-Int_t n_cell; //numero di cella in cui cade l'ELETTRONE
-Int_t n_cell_ph; //numero di cella in cui cade il fotone
-Int_t n_tot=0;
-
-Double_t same_cell=0;
-Double_t different_cell=0;
-Double_t E_CAL;
 Double_t Rm = 1.959 ; //raggio di Moliere in centimetri 
     
-TF2 *fxy = new TF2("fxy","(1/(2*Pi()*[0]*[1]))*(Exp(((x-[2])*(x-[2])+(y-[3])*(y-[3]))/(2*[0]*[1])))",-7.125,7.125,-7.125,7.125 );
+TF2 *fxy = new TF2("fxy","(1/(2*TMath::pi()*[0]*[1]))*(TMath::exp(((x-[2])*(x-[2])+(y-[3])*(y-[3]))/(2*[0]*[1])))",-7.125,7.125,-7.125,7.125 );
  
 
     fxy->SetParameter(0,Rm);
     fxy->SetParameter(1,Rm);
     fxy->SetParameter(2,1);
     fxy->SetParameter(3,3);
-          fxy->Draw(); 
+    fxy->Draw(); 
 }

04129e8f6938225db7b420608c06bf498fff4c33
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-10-12 16:08:24 +0200

    ecal

--- ECAL.C
+++ ECAL.C
@@ -15,10 +15,13 @@ Double_t Rm = 1.959 ; //raggio di Moliere in centimetri
     
 TF2 *fxy = new TF2("fxy","(1/(2*3.14159265358979323846*[0]*[1]))*(exp(((x-[2])*(x-[2])+(y-[3])*(y-[3]))/(2*[0]*[1])))",-7.125,7.125,-7.125,7.125 );
  
+TCanvas * ER= new TCanvas("ER","ER",1000,1000,2000,2500);
 
     fxy->SetParameter(0,Rm);
     fxy->SetParameter(1,Rm);
     fxy->SetParameter(2,1);
     fxy->SetParameter(3,3);
     fxy->Draw(); 
+    
+    ER->SaveAs("functionE.png");
 }

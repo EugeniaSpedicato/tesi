c0b70fd7e6e74d82ca882c353018f7998ca8b5e6
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-10-14 16:47:31 +0200

    mod cal

--- cal.C
+++ cal.C
@@ -38,9 +38,7 @@ Double_t Rm = 1.959 ; //raggio di Moliere in centimetri
        
         detKinBeamRot_cooXe=detKinBeamRot_cooXe*100;
         detKinBeamRot_cooYe=detKinBeamRot_cooYe*100;
-        detKinBeamRot_cooXmu=detKinBeamRot_cooXmu*100;
-        detKinBeamRot_cooYmu=detKinBeamRot_cooYmu*100;
-        photon_coox=photon_coox*100;
+           photon_coox=photon_coox*100;
         photon_cooy=photon_cooy*100;
         if (photon_coox!=-1 && photon_cooy!=-1)
     {  
@@ -53,211 +51,17 @@ Double_t Rm = 1.959 ; //raggio di Moliere in centimetri
        
     Double_t d_e_mu=sqrt( (detKinBeamRot_cooXe-detKinBeamRot_cooXmu)*(detKinBeamRot_cooXe-detKinBeamRot_cooXmu)+(detKinBeamRot_cooYe-detKinBeamRot_cooYmu)*(detKinBeamRot_cooYe-detKinBeamRot_cooYmu) ); 
        
-
-if (abs(detKinBeamRot_cooXe)<7.125 && abs(detKinBeamRot_cooYe)<7.125)
-    
-{  
-    if (detKinBeamRot_cooXe>-7.125 && detKinBeamRot_cooXe<-4.275 && detKinBeamRot_cooYe<7.125 && detKinBeamRot_cooYe>4.275) {n_cell=1;}
-    
-    if (detKinBeamRot_cooXe>-4.275 && detKinBeamRot_cooXe<-1.425 && detKinBeamRot_cooYe<7.125 && detKinBeamRot_cooYe>4.275) {n_cell=2;}
-
-    if (detKinBeamRot_cooXe>-1.425 && detKinBeamRot_cooXe<1.425 && detKinBeamRot_cooYe<7.125 && detKinBeamRot_cooYe>4.275) {n_cell=3;}
-
-    if (detKinBeamRot_cooXe>1.425 && detKinBeamRot_cooXe<4.275 && detKinBeamRot_cooYe<7.125 && detKinBeamRot_cooYe>4.275) {n_cell=4;}
-
-    if (detKinBeamRot_cooXe>4.275 && detKinBeamRot_cooXe<7.125 && detKinBeamRot_cooYe<7.125 && detKinBeamRot_cooYe>4.275) {n_cell=5;}
-    
-    
-    if (detKinBeamRot_cooXe>-7.125 && detKinBeamRot_cooXe<-4.275 && detKinBeamRot_cooYe<4.275 && detKinBeamRot_cooYe>1.425) {n_cell=6;}
-    
-    if (detKinBeamRot_cooXe>-4.275 && detKinBeamRot_cooXe<-1.425 && detKinBeamRot_cooYe<4.275 && detKinBeamRot_cooYe>1.425) {n_cell=7;}
-
-    if (detKinBeamRot_cooXe>-1.425 && detKinBeamRot_cooXe<1.425 && detKinBeamRot_cooYe<4.275 && detKinBeamRot_cooYe>1.425) {n_cell=8;}
-
-    if (detKinBeamRot_cooXe>1.425 && detKinBeamRot_cooXe<4.275 && detKinBeamRot_cooYe<4.275 && detKinBeamRot_cooYe>1.425) {n_cell=9;}
-
-    if (detKinBeamRot_cooXe>4.275 && detKinBeamRot_cooXe<7.125 && detKinBeamRot_cooYe<4.275 && detKinBeamRot_cooYe>1.425) {n_cell=10;}
-    
-
-    
-    if (detKinBeamRot_cooXe>-7.125 && detKinBeamRot_cooXe<-4.275 && detKinBeamRot_cooYe<1.425 && detKinBeamRot_cooYe>-1.425) {n_cell=11;}
-    
-    if (detKinBeamRot_cooXe>-4.275 && detKinBeamRot_cooXe<-1.425 && detKinBeamRot_cooYe<1.425 && detKinBeamRot_cooYe>-1.425) {n_cell=12;}
-
-    if (detKinBeamRot_cooXe>-1.425 && detKinBeamRot_cooXe<1.425 && detKinBeamRot_cooYe<1.425 && detKinBeamRot_cooYe>-1.425) {n_cell=13;}
-
-    if (detKinBeamRot_cooXe>1.425 && detKinBeamRot_cooXe<4.275 && detKinBeamRot_cooYe<1.425 && detKinBeamRot_cooYe>-1.425) {n_cell=14;}
-
-    if (detKinBeamRot_cooXe>4.275 && detKinBeamRot_cooXe<7.125 && detKinBeamRot_cooYe<1.425 && detKinBeamRot_cooYe>-1.425) {n_cell=15;}
-    
-    
-    
-    
-    if (detKinBeamRot_cooXe>-7.125 && detKinBeamRot_cooXe<-4.275 && detKinBeamRot_cooYe<-1.425 && detKinBeamRot_cooYe>-4.275) {n_cell=16;}
-    
-    if (detKinBeamRot_cooXe>-4.275 && detKinBeamRot_cooXe<-1.425 && detKinBeamRot_cooYe<-1.425 && detKinBeamRot_cooYe>-4.275) {n_cell=17;}
-
-    if (detKinBeamRot_cooXe>-1.425 && detKinBeamRot_cooXe<1.425 && detKinBeamRot_cooYe<-1.425 && detKinBeamRot_cooYe>-4.275) {n_cell=18;}
-
-    if (detKinBeamRot_cooXe>1.425 && detKinBeamRot_cooXe<4.275 && detKinBeamRot_cooYe<-1.425 && detKinBeamRot_cooYe>-4.275) {n_cell=19;}
-
-    if (detKinBeamRot_cooXe>4.275 && detKinBeamRot_cooXe<7.125 && detKinBeamRot_cooYe<-1.425 && detKinBeamRot_cooYe>-4.275) {n_cell=20;}
-    
-    
-    
-    if (detKinBeamRot_cooXe>-7.125 && detKinBeamRot_cooXe<-4.275 && detKinBeamRot_cooYe<-4.275 && detKinBeamRot_cooYe>-7.125) {n_cell=21;}
-    
-    if (detKinBeamRot_cooXe>-4.275 && detKinBeamRot_cooXe<-1.425 && detKinBeamRot_cooYe<-4.275 && detKinBeamRot_cooYe>-7.125) {n_cell=22;}
-
-    if (detKinBeamRot_cooXe>-1.425 && detKinBeamRot_cooXe<1.425 && detKinBeamRot_cooYe<-4.275 && detKinBeamRot_cooYe>-7.125) {n_cell=23;}
-
-    if (detKinBeamRot_cooXe>1.425 && detKinBeamRot_cooXe<4.275 && detKinBeamRot_cooYe<-4.275 && detKinBeamRot_cooYe>-7.125) {n_cell=24;}
-
-    if (detKinBeamRot_cooXe>4.275 && detKinBeamRot_cooXe<7.125 && detKinBeamRot_cooYe<-4.275 && detKinBeamRot_cooYe>-7.125) {n_cell=25;}
-    
        
-       
-if (abs(photon_coox)<7.125 && abs(photon_cooy)<7.125 && photon_energy>0.2 && d_e_ph>1*Rm)
-    
-{ n_tot += wgt_full;
- 
-    if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=1;}
-    
-    if (photon_coox>-4.275 && photon_coox<-1.425 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=2;}
-
-    if (photon_coox>-1.425 && photon_coox<1.425 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=3;}
-
-    if (photon_coox>1.425 && photon_coox<4.275 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=4;}
-
-    if (photon_coox>4.275 && photon_coox<7.125 && photon_cooy<7.125 && photon_cooy>4.275) {n_cell_ph=5;}
-    
-    
-    if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<4.275 && photon_cooy>1.425) {n_cell_ph=6;}
-    
-    if (photon_coox>-4.275 && photon_coox<-1.425 && photon_cooy<4.275 && photon_cooy>1.425) {n_cell_ph=7;}
-
-    if (photon_coox>-1.425 && photon_coox<1.425 && photon_cooy<4.275 && photon_cooy>1.425) {n_cell_ph=8;}
-
-    if (photon_coox>1.425 && photon_coox<4.275 && photon_cooy<4.275 && photon_cooy>1.425) {n_cell_ph=9;}
-
-    if (photon_coox>4.275 && photon_coox<7.125 && photon_cooy<4.275 && photon_cooy>1.425) {n_cell_ph=10;}
-    
-
-    
-    if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<1.425 && photon_cooy>-1.425) {n_cell_ph=11;}
-    
-    if (photon_coox>-4.275 && photon_coox<-1.425 && photon_cooy<1.425 && photon_cooy>-1.425) {n_cell_ph=12;}
-
-    if (photon_coox>-1.425 && photon_coox<1.425 && photon_cooy<1.425 && photon_cooy>-1.425) {n_cell_ph=13;}
-
-    if (photon_coox>1.425 && photon_coox<4.275 && photon_cooy<1.425 && photon_cooy>-1.425) {n_cell_ph=14;}
-
-    if (photon_coox>4.275 && photon_coox<7.125 && photon_cooy<1.425 && photon_cooy>-1.425) {n_cell_ph=15;}
-    
-    
-    
-    
-    if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<-1.425 && photon_cooy>-4.275) {n_cell_ph=16;}
-    
-    if (photon_coox>-4.275 && photon_coox<-1.425 && photon_cooy<-1.425 && photon_cooy>-4.275) {n_cell_ph=17;}
-
-    if (photon_coox>-1.425 && photon_coox<1.425 && photon_cooy<-1.425 && photon_cooy>-4.275) {n_cell_ph=18;}
-
-    if (photon_coox>1.425 && photon_coox<4.275 && photon_cooy<-1.425 && photon_cooy>-4.275) {n_cell_ph=19;}
-
-    if (photon_coox>4.275 && photon_coox<7.125 && photon_cooy<-1.425 && photon_cooy>-4.275) {n_cell_ph=20;}
-    
-    
-    
-    if (photon_coox>-7.125 && photon_coox<-4.275 && photon_cooy<-4.275 && photon_cooy>-7.125) {n_cell_ph=21;}
-    
-    if (photon_coox>-4.275 && photon_coox<-1.425 && photon_cooy<-4.275 && photon_cooy>-7.125) {n_cell_ph=22;}
-
-    if (photon_coox>-1.425 && photon_coox<1.425 && photon_cooy<-4.275 && photon_cooy>-7.125) {n_cell_ph=23;}
-
-    if (photon_coox>1.425 && photon_coox<4.275 && photon_cooy<-4.275 && photon_cooy>-7.125) {n_cell_ph=24;}
-
-    if (photon_coox>4.275 && photon_coox<7.125 && photon_cooy<-4.275 && photon_cooy>-7.125) {n_cell_ph=25;}
- 
-if (n_cell_ph==n_cell)
-{same_cell += wgt_full;}
-else {different_cell += wgt_full;}  // contatori double 
-} 
-      
-/*if (abs(detKinBeamRot_cooXmu)<7.125 && abs(detKinBeamRot_cooYmu)<7.125 && d_e_mu>3*Rm)
-    
-{ n_tot+=wgt_full;
-    if (detKinBeamRot_cooXmu>-7.125 && detKinBeamRot_cooXmu<-4.275 && detKinBeamRot_cooYmu<7.125 && detKinBeamRot_cooYmu>4.275) {n_cell_ph=1;}
-    
-    if (detKinBeamRot_cooXmu>-4.275 && detKinBeamRot_cooXmu<-1.425 && detKinBeamRot_cooYmu<7.125 && detKinBeamRot_cooYmu>4.275) {n_cell_ph=2;}
-
-    if (detKinBeamRot_cooXmu>-1.425 && detKinBeamRot_cooXmu<1.425 && detKinBeamRot_cooYmu<7.125 && detKinBeamRot_cooYmu>4.275) {n_cell_ph=3;}
-
-    if (detKinBeamRot_cooXmu>1.425 && detKinBeamRot_cooXmu<4.275 && detKinBeamRot_cooYmu<7.125 && detKinBeamRot_cooYmu>4.275) {n_cell_ph=4;}
-
-    if (detKinBeamRot_cooXmu>4.275 && detKinBeamRot_cooXmu<7.125 && detKinBeamRot_cooYmu<7.125 && detKinBeamRot_cooYmu>4.275) {n_cell_ph=5;}
-    
-    
-    if (detKinBeamRot_cooXmu>-7.125 && detKinBeamRot_cooXmu<-4.275 && detKinBeamRot_cooYmu<4.275 && detKinBeamRot_cooYmu>1.425) {n_cell_ph=6;}
-    
-    if (detKinBeamRot_cooXmu>-4.275 && detKinBeamRot_cooXmu<-1.425 && detKinBeamRot_cooYmu<4.275 && detKinBeamRot_cooYmu>1.425) {n_cell_ph=7;}
-
-    if (detKinBeamRot_cooXmu>-1.425 && detKinBeamRot_cooXmu<1.425 && detKinBeamRot_cooYmu<4.275 && detKinBeamRot_cooYmu>1.425) {n_cell_ph=8;}
-
-    if (detKinBeamRot_cooXmu>1.425 && detKinBeamRot_cooXmu<4.275 && detKinBeamRot_cooYmu<4.275 && detKinBeamRot_cooYmu>1.425) {n_cell_ph=9;}
-
-    if (detKinBeamRot_cooXmu>4.275 && detKinBeamRot_cooXmu<7.125 && detKinBeamRot_cooYmu<4.275 && detKinBeamRot_cooYmu>1.425) {n_cell_ph=10;}
-    
-
-    
-    if (detKinBeamRot_cooXmu>-7.125 && detKinBeamRot_cooXmu<-4.275 && detKinBeamRot_cooYmu<1.425 && detKinBeamRot_cooYmu>-1.425) {n_cell_ph=11;}
-    
-    if (detKinBeamRot_cooXmu>-4.275 && detKinBeamRot_cooXmu<-1.425 && detKinBeamRot_cooYmu<1.425 && detKinBeamRot_cooYmu>-1.425) {n_cell_ph=12;}
-
-    if (detKinBeamRot_cooXmu>-1.425 && detKinBeamRot_cooXmu<1.425 && detKinBeamRot_cooYmu<1.425 && detKinBeamRot_cooYmu>-1.425) {n_cell_ph=13;}
-
-    if (detKinBeamRot_cooXmu>1.425 && detKinBeamRot_cooXmu<4.275 && detKinBeamRot_cooYmu<1.425 && detKinBeamRot_cooYmu>-1.425) {n_cell_ph=14;}
-
-    if (detKinBeamRot_cooXmu>4.275 && detKinBeamRot_cooXmu<7.125 && detKinBeamRot_cooYmu<1.425 && detKinBeamRot_cooYmu>-1.425) {n_cell_ph=15;}
-    
-    
-    
-    
-    if (detKinBeamRot_cooXmu>-7.125 && detKinBeamRot_cooXmu<-4.275 && detKinBeamRot_cooYmu<-1.425 && detKinBeamRot_cooYmu>-4.275) {n_cell_ph=16;}
-    
-    if (detKinBeamRot_cooXmu>-4.275 && detKinBeamRot_cooXmu<-1.425 && detKinBeamRot_cooYmu<-1.425 && detKinBeamRot_cooYmu>-4.275) {n_cell_ph=17;}
-
-    if (detKinBeamRot_cooXmu>-1.425 && detKinBeamRot_cooXmu<1.425 && detKinBeamRot_cooYmu<-1.425 && detKinBeamRot_cooYmu>-4.275) {n_cell_ph=18;}
-
-    if (detKinBeamRot_cooXmu>1.425 && detKinBeamRot_cooXmu<4.275 && detKinBeamRot_cooYmu<-1.425 && detKinBeamRot_cooYmu>-4.275) {n_cell_ph=19;}
-
-    if (detKinBeamRot_cooXmu>4.275 && detKinBeamRot_cooXmu<7.125 && detKinBeamRot_cooYmu<-1.425 && detKinBeamRot_cooYmu>-4.275) {n_cell_ph=20;}
-    
-    
-    
-    if (detKinBeamRot_cooXmu>-7.125 && detKinBeamRot_cooXmu<-4.275 && detKinBeamRot_cooYmu<-4.275 && detKinBeamRot_cooYmu>-7.125) {n_cell_ph=21;}
-    
-    if (detKinBeamRot_cooXmu>-4.275 && detKinBeamRot_cooXmu<-1.425 && detKinBeamRot_cooYmu<-4.275 && detKinBeamRot_cooYmu>-7.125) {n_cell_ph=22;}
-
-    if (detKinBeamRot_cooXmu>-1.425 && detKinBeamRot_cooXmu<1.425 && detKinBeamRot_cooYmu<-4.275 && detKinBeamRot_cooYmu>-7.125) {n_cell_ph=23;}
-
-    if (detKinBeamRot_cooXmu>1.425 && detKinBeamRot_cooXmu<4.275 && detKinBeamRot_cooYmu<-4.275 && detKinBeamRot_cooYmu>-7.125) {n_cell_ph=24;}
-
-    if (detKinBeamRot_cooXmu>4.275 && detKinBeamRot_cooXmu<7.125 && detKinBeamRot_cooYmu<-4.275 && detKinBeamRot_cooYmu>-7.125) {n_cell_ph=25;}
- 
-if (n_cell_ph==n_cell)
+if (d_e_ph>1*Rm)
+{
+    if (photon_n_cell_ph==detKinBeamRot_n_cell_e)
 {same_cell+=wgt_full;}
-else {different_cell+=wgt_full;} 
-}*/
+else {different_cell+=wgt_full;}  // contatori double 
+}
+
  
 }
-       
 
-
-       
-       
-   }
-}
     
  cout << "Elettroni e fotoni nella stessa cella: " << same_cell << endl;
  cout << "Elettroni e fotoni in una diversa cella: " << different_cell << endl;   

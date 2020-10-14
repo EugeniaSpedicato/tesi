252c5cf7a8a8918641748f80f9639e816ee22f95
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2020-10-14 09:30:14 +0200

    prova 2

--- ECAL.C
+++ ECAL.C
@@ -309,11 +309,11 @@ E[2][22]= E[0][22]; //cella 23
 E[2][23]= E[0][23]; //cella 24
 E[2][24]= E[0][24]; //cella 25 
         }
-    if (jentry==2) {for (Int_t i=0;i<25;i++){cout << "Energia nella cella " << i <<" : " << E[3][i] << endl;}}
+    if (jentry==2) {for (Int_t i=0;i<25;i++){cout << "Energia nella cella " << i <<" : " << E[2][i] << endl;}}
     
    }
        
-  void upd() { TFile *f = new TFile("hs.root","update"); TTree *T = (TTree*)f->Get("ntuple"); float px,py; float pt; TBranch *bpt = T->Branch("pt",&pt,"pt/F"); T->SetBranchAddress("px",&px); T->SetBranchAddress("py",&py); Long64_t nentries = T->GetEntries(); for (Long64_t i=0;i<nentries;i++) { T->GetEntry(i); pt = TMath::Sqrt(px*px+py*py); bpt->Fill(); } T->Print(); T->Write(); delete f; }
+
        
        
 }

e9b9cea3c7547eb33b9dd211c23f2a7aa3e41e7e
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-13 15:56:27 +0100

    modifica

--- test/test_MuE.sh
+++ test/test_MuE.sh
@@ -4,7 +4,6 @@ MAIN=MuE
 
 ROOTINCDIR=`$ROOTSYS/bin/root-config --incdir`
 ROOTLIBS=`$ROOTSYS/bin/root-config --cflags --libs`
-CXXFLGS=`$ROOTSYS/root-config --cflags'
 
 code=../code
 
@@ -20,8 +19,8 @@ gfortran ${FOPTIONS} ${code}/muerad.f
 OPTIONS="-O2 -Wall"
 OPTIONS="-g -Wall"
 
-g++ ${OPTIONS} -I${code} -I${ROOTINCDIR} -I${ROOTLIBS} -I${CXXFLGS} ${code}/${MAIN}.cc MuEtreeDict.C ${code}/MuEtree.cc ${code}/Inputs.cc ${code}/Analysis.cc ${code}/FastSim.cc ${code}/Utils.cc ${code}/ElasticState.cc ${code}/dalpha.cc ${code}/ECAL.cc ${code}/EMShower.cc ${code}/RadialInterval.cc ${code}/GammaFunctionGenerator.cc ${code}/BaseNumericalRandomGenerator.cc ${code}/IncGamma.cc
-summa.o hadr5n12.o muerad.o -lgfortran -lX11 -o ${MAIN}.exe
+g++ ${OPTIONS} -I${code} -I${ROOTINCDIR} ${code}/${MAIN}.cc MuEtreeDict.C ${code}/MuEtree.cc ${code}/Inputs.cc ${code}/Analysis.cc ${code}/FastSim.cc ${code}/Utils.cc ${code}/ElasticState.cc ${code}/dalpha.cc ${code}/ECAL.cc ${code}/EMShower.cc ${code}/RadialInterval.cc ${code}/GammaFunctionGenerator.cc ${code}/BaseNumericalRandomGenerator.cc ${code}/IncGamma.cc
+summa.o hadr5n12.o muerad.o ${ROOTLIBS} -lgfortran -lX11 -o ${MAIN}.exe
 
 #exit
 

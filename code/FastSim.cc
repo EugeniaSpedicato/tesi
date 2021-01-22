34ad4ca69dea6f7d65880a8c951a06f7325b47d4
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-22 15:42:34 +0100

    time

--- code/FastSim.cc
+++ code/FastSim.cc
@@ -124,15 +124,17 @@ double energy_sm_el=p_e_out_div_smeared.E();
 
 myGrid->CreateGrid(5,-7.125,7.125,5,-7.125,7.125);
     
-    nPart=1;
-    X0depth=0.;
-    coo_el.push_back(coo[2][0]*100);//cm
-    coo_el.push_back(coo[3][0]*100);//cm
-    energy_in_el.push_back(energy_sm_el);
-    myGrid->SetEnergy(energy_sm_el);
-    EMShower TheShowerEl(gamma,myParam,myGrid,bFixedLength,nPart,X0depth,energy_in_el,coo_el);
-    TheShowerEl.compute();    
-    LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot); 
+nPart=1;
+X0depth=0.;
+coo_el.push_back(coo[2][0]*100);//cm
+coo_el.push_back(coo[3][0]*100);//cm
+energy_in_el.push_back(energy_sm_el);
+myGrid->SetEnergy(energy_sm_el);
+EMShower TheShowerEl(gamma,myParam,myGrid,bFixedLength,nPart,X0depth,energy_in_el,coo_el);
+TheShowerEl.compute();   
+    
+LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot); 
+    
 auto n_photons = event.photons.size();     
 if (n_photons>0){  
  PxPyPzEVector p_gamma_Lab = {
@@ -141,13 +143,11 @@ if (n_photons>0){
 				 event.photons[0].pz,
 			     event.photons[0].E};
 
-PxPyPzEVector p_gamma_Lab_div = RotDiv(p_mu_in,p_gamma_Lab);
-    
-double en_ph_sm=p_gamma_Lab_div.E();
-    
+PxPyPzEVector p_gamma_Lab_div = RotDiv(p_mu_in,p_gamma_Lab);   
+double en_ph_sm=p_gamma_Lab_div.E();  
 TMatrixD cooPH=MCSphoton(p_gamma_Lab_div,xin,yin);
-
 double ECAL_E= energy_sm_el+en_ph_sm;   
+    
     if (ECAL_E>1)
     {
     //for electrons
@@ -216,7 +216,7 @@ LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared,
 }*/
   
 myGrid->Draw_ECAL(i); 
-vector<double> Ecell=myGrid->EnergyContent(); 
+/*vector<double> Ecell=myGrid->EnergyContent(); 
 detKinBeamRot.Ecell1=Ecell[0];
 detKinBeamRot.Ecell2=Ecell[1];    
 detKinBeamRot.Ecell3=Ecell[2];    
@@ -241,7 +241,7 @@ detKinBeamRot.Ecell21=Ecell[20];
 detKinBeamRot.Ecell22=Ecell[21];    
 detKinBeamRot.Ecell23=Ecell[22];    
 detKinBeamRot.Ecell24=Ecell[23];    
-detKinBeamRot.Ecell25=Ecell[24];    
+detKinBeamRot.Ecell25=Ecell[24]; */   
 }
 
 

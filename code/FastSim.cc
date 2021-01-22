f15227d4d62eb924d00f4baeed7a2acb251f7dc3
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-22 16:20:19 +0100

    time

--- code/FastSim.cc
+++ code/FastSim.cc
@@ -123,15 +123,16 @@ double energy_sm_el=p_e_out_div_smeared.E();
 //otherwise create it inside the shower.
 
 myGrid->CreateGrid(5,-7.125,7.125,5,-7.125,7.125);
-    
-nPart=1;
+
+if (energy_sm_el>1)    
+{nPart=1;
 X0depth=0.;
 coo_el.push_back(coo[2][0]*100);//cm
 coo_el.push_back(coo[3][0]*100);//cm
 energy_in_el.push_back(energy_sm_el);
 myGrid->SetEnergy(energy_sm_el);
 EMShower TheShowerEl(gamma,myParam,myGrid,bFixedLength,nPart,X0depth,energy_in_el,coo_el);
-TheShowerEl.compute();   
+TheShowerEl.compute(); }  
     
 LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot); 
     
@@ -148,7 +149,7 @@ double en_ph_sm=p_gamma_Lab_div.E();
 TMatrixD cooPH=MCSphoton(p_gamma_Lab_div,xin,yin);
 double ECAL_E= energy_sm_el+en_ph_sm;   
     
-    if (ECAL_E>10)
+    if (ECAL_E>1)
     {
     //for electrons
 /*    nPart=1;

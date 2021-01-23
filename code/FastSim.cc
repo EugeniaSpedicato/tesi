52e0e437aab0a2bbc69c8e3c73871bfc1971c7aa
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-23 09:56:26 +0100

    code

--- code/FastSim.cc
+++ code/FastSim.cc
@@ -125,6 +125,7 @@ double energy_sm_el=p_e_out_div_smeared.E();
 myGrid->CreateGrid(5,-7.125,7.125,5,-7.125,7.125);
 
 LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot); 
+LoadPhoton(event, photon,p_gamma_Lab_div,cooPH[0][0],cooPH[0][1]);
     
 auto n_photons = event.photons.size();     
 if (n_photons>0){  
@@ -163,8 +164,6 @@ double ECAL_E= energy_sm_el+en_ph_sm;
     EMShower TheShowerPh(gamma,myParam,myGrid,bFixedLength,nPart,X0depth,energy_in_ph,coo_ph);
     TheShowerPh.compute();
     cout << "esco dalla shower fotonica N " << i <<endl;}
-  
-LoadPhoton(event, photon,p_gamma_Lab_div,cooPH[0][0],cooPH[0][1]);
  }
 else {    
 //for electrons
@@ -177,13 +176,11 @@ energy_in_el.push_back(energy_sm_el);
 myGrid->SetEnergy(energy_sm_el);
 EMShower TheShower(gamma,myParam,myGrid,bFixedLength,nPart,X0depth,energy_in_el,coo_el);
 TheShower.compute();
-
-}
-LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot);    
+}  
 }
   
 myGrid->Draw_ECAL(i); 
-/*vector<double> Ecell=myGrid->EnergyContent(); 
+vector<double> Ecell=myGrid->EnergyContent(); 
 detKinBeamRot.Ecell1=Ecell[0];
 detKinBeamRot.Ecell2=Ecell[1];    
 detKinBeamRot.Ecell3=Ecell[2];    
@@ -208,7 +205,7 @@ detKinBeamRot.Ecell21=Ecell[20];
 detKinBeamRot.Ecell22=Ecell[21];    
 detKinBeamRot.Ecell23=Ecell[22];    
 detKinBeamRot.Ecell24=Ecell[23];    
-detKinBeamRot.Ecell25=Ecell[24]; */   
+detKinBeamRot.Ecell25=Ecell[24];   
 }
 
 

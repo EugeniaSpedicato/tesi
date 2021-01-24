6fa42925c27263454730efdbdcffa066a60ae981
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-24 14:50:43 +0100

    ncell

--- cal.C
+++ cal.C
@@ -17,11 +17,10 @@ void atree::Loop()
     typedef map<int, double>  energy_cell; 
     energy_cell en_c;    
     
-Int_t n_cell; //numero di cella in cui cade l'ELETTRONE
-Int_t n_cell_ph; //numero di cella in cui cade il fotone
+
 Double_t n_tot_eph=0.;
     
-    Double_t n_tot=0.;
+   /* Double_t n_tot=0.;
     Double_t n_one=0.;
     Double_t n_two=0.;
     Double_t ratio=0.; // ratio of two clusters over total, events to drop
@@ -49,15 +48,16 @@ Double_t n_tot_eph=0.;
     Double_t n_tot_cut1=0.; //numero di elettroni nel calorimetro
     Double_t n_two_cut1=0.; //numero di elettroni che hanno una distanza maggiore di 2RM dal fotone, quindi 2 clusters
     Double_t n_one_cut1=0.; //casi rimanenti che formano 1 cluster
-    Double_t ratio_cut1=0.;
+    Double_t ratio_cut1=0.;*/
 
         
 
 Double_t same_cell=0.;
 Double_t different_cell=0.;
-Double_t E_CAL;
-Double_t Rm = 1.959 ; //raggio di Moliere in centimetri    
- 
+//Double_t E_CAL;
+Double_t Rm = 2.190 ; //raggio di Moliere in centimetri    
+Double_t E9=0.;
+TH1F* hist_E9=new TH1F("E9", "E9", 100,0,1);
 
     
     if (fChain == 0) return;
@@ -72,43 +72,42 @@ Double_t Rm = 1.959 ; //raggio di Moliere in centimetri
       nb = fChain->GetEntry(jentry);   nbytes += nb;
        
        
-        en_c[1]=detKinBeamRot_Ecell1; en_c[2]=37; en_c[3]=38; en_c[4]=39; en_c[5]=40;
-        en_c[6]=detKinBeamRot_Ecell6; en_c[7]=30; en_c[8]=31; en_c[9]=32; en_c[10]=33;
-        en_c[11]=22; en_c[12]=23; en_c[13]=24; en_c[14]=25; en_c[15]=26;
-        en_c[16]=15; en_c[17]=16; en_c[18]=17; en_c[19]=18; en_c[20]=19;
-        en_c[21]=8; en_c[22]=9; en_c[23]=10; en_c[24]=11; en_c[25]=12;
+        en_c[1]=detKinBeamRot_Ecell1; en_c[2]=detKinBeamRot_Ecell2; en_c[3]=detKinBeamRot_Ecell3; en_c[4]=detKinBeamRot_Ecell4; en_c[5]=detKinBeamRot_Ecell5;
+        en_c[6]=detKinBeamRot_Ecell6; en_c[7]=detKinBeamRot_Ecell7; en_c[8]=detKinBeamRot_Ecell8; en_c[9]=detKinBeamRot_Ecell9; en_c[10]=detKinBeamRot_Ecell10;
+        en_c[11]=detKinBeamRot_Ecell11; en_c[12]=detKinBeamRot_Ecell12; en_c[13]=detKinBeamRot_Ecell13; en_c[14]=detKinBeamRot_Ecell14; en_c[15]=detKinBeamRot_Ecell15;
+        en_c[16]=detKinBeamRot_Ecell16; en_c[17]=detKinBeamRot_Ecell17; en_c[18]=detKinBeamRot_Ecell18; en_c[19]=detKinBeamRot_Ecell19; en_c[20]=detKinBeamRot_Ecell20;
+        en_c[21]=detKinBeamRot_Ecell21; en_c[22]detKinBeamRot_Ecell22; en_c[23]=detKinBeamRot_Ecell23; en_c[24]=detKinBeamRot_Ecell24; en_c[25]=detKinBeamRot_Ecell25;
        
+
+        detKinBeamRot_cooXe=detKinBeamRot_cooXe*100; // cm
+        detKinBeamRot_cooYe=detKinBeamRot_cooYe*100; // cm
+        photon_coox=photon_coox*100; // cm
+        photon_cooy=photon_cooy*100; // cm
        
+       E9=detKinBeamRot_n_max_Cell/detKinBeamRot_E_clus3x3;
        
-       
-        detKinBeamRot_cooXe=detKinBeamRot_cooXe*100;
-        detKinBeamRot_cooYe=detKinBeamRot_cooYe*100;
-           photon_coox=photon_coox*100;
-        photon_cooy=photon_cooy*100;
-    if (photon_coox!=-1 && photon_cooy!=-1)
+    /*if (photon_coox!=-1 && photon_cooy!=-1)
     {  
      E_CAL=detKinBeamRot_Ee+photon_energy;}
     else 
-    {  E_CAL=detKinBeamRot_Ee;}
+    {  E_CAL=detKinBeamRot_Ee;}*/
        
        
-    Double_t d_e_ph=sqrt( (detKinBeamRot_cooXe-photon_coox)*(detKinBeamRot_cooXe-photon_coox)+(detKinBeamRot_cooYe-photon_cooy)*(detKinBeamRot_cooYe-photon_cooy) ); 
+   /* Double_t d_e_ph=sqrt( (detKinBeamRot_cooXe-photon_coox)*(detKinBeamRot_cooXe-photon_coox)+(detKinBeamRot_cooYe-photon_cooy)*(detKinBeamRot_cooYe-photon_cooy) ); 
        
-    Double_t d_e_mu=sqrt( (detKinBeamRot_cooXe-detKinBeamRot_cooXmu)*(detKinBeamRot_cooXe-detKinBeamRot_cooXmu)+(detKinBeamRot_cooYe-detKinBeamRot_cooYmu)*(detKinBeamRot_cooYe-detKinBeamRot_cooYmu) ); 
+    Double_t d_e_mu=sqrt( (detKinBeamRot_cooXe-detKinBeamRot_cooXmu)*(detKinBeamRot_cooXe-detKinBeamRot_cooXmu)+(detKinBeamRot_cooYe-detKinBeamRot_cooYmu)*(detKinBeamRot_cooYe-detKinBeamRot_cooYmu) ); */
        
-  if (abs(photon_coox)<0.0715 && abs(photon_cooy)<0.0715 && abs(detKinBeamRot_cooXe) < 0.07125 && abs(detKinBeamRot_cooYe) < 0.07125){ 
+  if (photon_n_cell_ph!=0 && detKinBeamRot_n_cell_e!=0)
+  {   
+      n_tot_eph+=wgt_full; // e+gamma sul calorimetro
+        if (photon_n_cell_ph==detKinBeamRot_n_cell_e) same_cell+=wgt_full; //stessa cella
+        else different_cell+=wgt_full;  // cella diversa
       
-if (d_e_ph>3*Rm)
-{   n_tot_eph+=wgt_full;
- if (photon_n_cell_ph==detKinBeamRot_n_cell_e)
-    {same_cell+=wgt_full;}
-    
-    else {different_cell+=wgt_full;}  // contatori double 
-}
+      hist_E9->Fill(E9,wgt_full);
   }
        
        
-if (detKinBeamRot_n_cell_e!=0)
+/*if (detKinBeamRot_n_cell_e!=0)
     {n_tot+=wgt_full;
 
 // SE IL FOTONE E' PRODOTTO DENTRO AL CALORIMETRO
@@ -256,17 +255,17 @@ ratio_cut=n_two_cut/n_tot_cut;
 ratio0=n_two0/n_tot0;
 ratio_cut0=n_two_cut0/n_tot_cut0; 
 ratio1=n_two1/n_tot1;
-ratio_cut1=n_two_cut1/n_tot_cut1;        
+ratio_cut1=n_two_cut1/n_tot_cut1;    */    
  
 }
 
     
  cout << "Elettroni e fotoni nella stessa cella: " << same_cell << endl;
  cout << "Elettroni e fotoni in una diversa cella: " << different_cell << endl;   
-cout << "n tot con fotoni= " << n_tot_eph << endl;
+cout << "n tot e+fotoni sul calorimetro = " << n_tot_eph << endl;
 cout << "-------------------------------------------"<<endl;
 
-cout << "Elettroni totali nel calorimetro: " << n_tot << endl;
+/*cout << "Elettroni totali nel calorimetro: " << n_tot << endl;
 cout << "Elettroni ad una distanza 2RM dal fotone: " << n_two << endl;
 cout << "Eventi in cui vedo solo un cluster: " << n_one << endl;
 cout << "Frazione di eventi scartabili: " << ratio <<endl;
@@ -298,6 +297,11 @@ cout << "-------------------------------------------"<<endl;
 cout << "Elettroni totali nel calorimetro CON TAGLIO TAR 1: " << n_tot_cut1 << endl;
 cout << "Elettroni ad una distanza 2RM dal fotone CON TAGLIO TAR 1: " << n_two_cut1 << endl;
 cout << "Eventi in cui vedo solo un cluster CON TAGLIO TAR 1: " << n_one_cut1 << endl;
-cout << "Frazione di eventi scartabili CON TAGLIO TAR 1: " << ratio_cut1 <<endl;
+cout << "Frazione di eventi scartabili CON TAGLIO TAR 1: " << ratio_cut1 <<endl;*/
+    
+TCanvas * c1= new TCanvas("c1","c1",1000,100,2500,2000);
+E9->GetXaxis()->SetTitle("Ecentral/E3x3");
+E9->Draw("HIST");   
+c1->SaveAs("E9.png");
     
 }

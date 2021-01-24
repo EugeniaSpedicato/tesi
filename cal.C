2634fbc8532e2fa0878fa6543ad34450420e8546
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-24 22:28:28 +0100

    ncell

--- cal.C
+++ cal.C
@@ -58,7 +58,7 @@ Double_t different_cell=0.;
 //Double_t E_CAL;
 Double_t Rm = 2.190 ; //raggio di Moliere in centimetri    
 Double_t E9=0.;
-TH1F* hist_E9=new TH1F("E9", "E9", 1000,0,1);
+TH1F* hist_E9=new TH1F("E9", "E9", 500,0.5,1);
 
 
     

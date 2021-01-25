705ebcc736762b03b9fb95bdfbd50d8e2cb34cd5
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-25 14:57:55 +0100

    cal

--- cal.C
+++ cal.C
@@ -60,7 +60,8 @@ Double_t different_cell=0.;
 Double_t E_CAL=0.;
 Double_t Rm = 2.190 ; //raggio di Moliere in centimetri    
 Double_t E9=0.;
-TH1F* hist_E9_eph=new TH1F("E9eph", "E9 e+ph", 500,0,1);
+TH1F* hist_E9_eph_same=new TH1F("E9eph", "E9 e+ph same cell", 500,0,1);
+TH1F* hist_E9_eph_diff=new TH1F("E9eph", "E9 e+ph different cell", 500,0,1);
 TH1F* hist_E9_e=new TH1F("E9e", "E9 e tot", 500,0,1);
 TH1F* hist_E9_NOph=new TH1F("E9noph", "E9 NO photons", 500,0,1);
 
@@ -132,8 +133,8 @@ if (detKinBeamRot_n_cell_e!=0)  {
       
       
         if (photon_n_cell_ph==detKinBeamRot_n_cell_e)
-        {same_cell+=wgt_full;      hist_E9_eph->Fill(E9,wgt_full);}//stessa cella
-        else {different_cell+=wgt_full;}  // cella diversa
+        {same_cell+=wgt_full;      hist_E9_eph_same->Fill(E9,wgt_full);}//stessa cella
+        else {different_cell+=wgt_full; hist_E9_eph_diff->Fill(E9,wgt_full);}  // cella diversa
   } else {
       n_tot_NOph+=wgt_full;   
       hist_E9_NOph->Fill(E9,wgt_full);
@@ -363,9 +364,13 @@ hist_Eout_9_e->GetXaxis()->SetTitle("Eout/E3x3");
 hist_Eout_9_e->SetLineWidth(3);
 hist_Eout_9_e->Draw("HIST"); 
 
-hist_Eout_9_eph->SetLineColor(kRed);
-hist_Eout_9_eph->SetLineWidth(3);
-hist_Eout_9_eph->Draw("HIST same"); 
+hist_Eout_9_eph_same->SetLineColor(kRed);
+hist_Eout_9_eph_same->SetLineWidth(3);
+hist_Eout_9_eph_same->Draw("HIST same"); 
+    
+hist_Eout_9_eph_diff->SetLineColor(kGreen);
+hist_Eout_9_eph_diff->SetLineWidth(3);
+hist_Eout_9_eph_diff->Draw("HIST same"); 
     
 hist_Eout_9_NOph->SetLineColor(kOrange);
 hist_Eout_9_NOph->SetLineWidth(3);

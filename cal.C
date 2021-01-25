df043176d254258cfbdb7e089aa48033a484a704
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-01-25 16:46:12 +0100

    cal

--- cal.C
+++ cal.C
@@ -417,8 +417,8 @@ c3->SaveAs("/home/LHCB-T3/espedicato/tesi/out+3x3.png");
 TCanvas * c4= new TCanvas("c4","c4",1000,100,2500,2000);
 E3x3noph->SetMarkerColor(kBlack);
 E3x3noph->Draw("AP");
-/*E3x3->SetMarkerColor(kRed);
-E3x3->Draw("AP SAME");*/
+E3x3->SetMarkerColor(kRed);
+E3x3->Draw("AP SAME");
 c4->SaveAs("/home/LHCB-T3/espedicato/tesi/thE.png");   
     
 }

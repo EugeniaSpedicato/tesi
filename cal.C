50f808b7ce6f163fb90de536879af25a31283879
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-09 15:20:09 +0100

    cal

--- cal.C
+++ cal.C
@@ -219,7 +219,7 @@ if (detKinBeamRot_n_cell_e!=0)
 if (detKinBeamRot_E_clus3x3!=0 ) {E3x3BIG->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_E_clus3x3,wgt_full);}
     
 if(abs(detKinBeamRot_cooXe)<4.275 && abs(detKinBeamRot_cooYe)<4.275) {
-    
+    cout << "n cella 3x3 " << detKinBeamRot_n_cell_e <<endl;
     n_tot_e+=wgt_full;
     hist_E9_e->Fill(E9,wgt_full);
     hist_thxz_e->Fill(anglex_e,wgt_full);
@@ -241,7 +241,9 @@ if(abs(detKinBeamRot_cooXe)<4.275 && abs(detKinBeamRot_cooYe)<4.275) {
     //hist_Eout_9_e->Fill(Eout_9,wgt_full);
     
     if (detKinBeamRot_E_clus3x3!=0) {E3x3->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_E_clus3x3,wgt_full);} //(detKinBeamRot_E_clus3x3!=0 && E9<0.95 && E9>0.5) 
-} else {  n_tot_e2P+=wgt_full;
+} else { 
+        cout << "n cella 5x5 " << detKinBeamRot_n_cell_e <<endl;
+    n_tot_e2P+=wgt_full;
     hist_E9_e2P->Fill(E9,wgt_full);
     hist_thxz_e2P->Fill(anglex_e,wgt_full);
     hist_thyz_e2P->Fill(angley_e,wgt_full);

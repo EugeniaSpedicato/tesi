8493c5a4ffe8f48d96f7e925ef795c8d54187bc9
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-13 17:03:39 +0100

    true

--- eff.C
+++ eff.C
@@ -70,9 +70,7 @@ Long64_t nentries = fChain->GetEntriesFast();
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
        
-       n_tot+=wgt_full;
-       ++n_tot_true;
-       
+
 
     if (photon_coox!=-1 && photon_cooy!=-1)
     {  
@@ -85,100 +83,47 @@ Long64_t nentries = fChain->GetEntriesFast();
         photon_coox=photon_coox*100; // cm
         photon_cooy=photon_cooy*100; // cm
        
-       E9=detKinBeamRot_E_1/detKinBeamRot_E_clus3x3;
-        Double_t anglex_e = atan2(detKinBeamRot_pXe_out, detKinBeamRot_pZe_out);
-        Double_t angley_e = atan2(detKinBeamRot_pYe_out, detKinBeamRot_pZe_out);
-
-
-The_true->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
-    if (detKinBeamRot_tar==0){The_true1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
-                              The_trueCUT1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
-    if (detKinBeamRot_tar==1){The_true2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
-                              The_trueCUT2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
-       
-The_trueCUT->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
-       
        
 if (detKinBeamRot_n_cell_e!=0 && E_CAL>0)
-{
-   
-        
-    n_tot_eBIG+=wgt_full;
-    ++n_tot_eBIG_true;
-    hist_E9_eBIG->Fill(E9,wgt_full);
-    hist_thxz_eBIG->Fill(anglex_e,wgt_full);
-    hist_thyz_eBIG->Fill(angley_e,wgt_full);
+{  
+
     TheBIG->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     TheBIGMCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     
     if (detKinBeamRot_tar==0)
-    {hist_thxz_e1BIG->Fill(anglex_e,wgt_full);
-    hist_thyz_e1BIG->Fill(angley_e,wgt_full);
-    TheBIG1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {TheBIG1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     TheBIG1MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
     if (detKinBeamRot_tar==1)
-    {hist_thxz_e2BIG->Fill(anglex_e,wgt_full);
-    hist_thyz_e2BIG->Fill(angley_e,wgt_full);
-    TheBIG2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {TheBIG2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     TheBIG2MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
-    
-    //hist_Eout_9_e->Fill(Eout_9,wgt_full);
-    
-if (detKinBeamRot_E_clus3x3!=0 ) {E3x3BIG->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_E_clus3x3,wgt_full);
-                                 E3x3true->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_Ee,wgt_full);}
+
     
 if(abs(detKinBeamRot_cooXe)<4.275 && abs(detKinBeamRot_cooYe)<4.275) {
-    n_tot_e+=wgt_full;
-    ++n_tot_e_true;
-    hist_E9_e->Fill(E9,wgt_full);
-    hist_thxz_e->Fill(anglex_e,wgt_full);
-    hist_thyz_e->Fill(angley_e,wgt_full);
+
     The->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     TheMCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     
     if (detKinBeamRot_tar==0)
-    {hist_thxz_e1->Fill(anglex_e,wgt_full);
-    hist_thyz_e1->Fill(angley_e,wgt_full);
-    The1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {The1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     The1MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
     if (detKinBeamRot_tar==1)
-    {hist_thxz_e2->Fill(anglex_e,wgt_full);
-    hist_thyz_e2->Fill(angley_e,wgt_full);
-    The2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {The2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     The2MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
-    
-    //hist_Eout_9_e->Fill(Eout_9,wgt_full);
-    
-    if (detKinBeamRot_E_clus3x3!=0) {
-        E3x3->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_E_clus3x3,wgt_full);
-        //E3x3true->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_Ee,wgt_full);
-    } //(detKinBeamRot_E_clus3x3!=0 && E9<0.95 && E9>0.5) 
+
 } else { 
-    n_tot_e2P+=wgt_full;
-    ++n_tot_e2P_true;
-    hist_E9_e2P->Fill(E9,wgt_full);
-    hist_thxz_e2P->Fill(anglex_e,wgt_full);
-    hist_thyz_e2P->Fill(angley_e,wgt_full);
+
     The2P->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     The2PMCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     
     if (detKinBeamRot_tar==0)
-    {hist_thxz_e12P->Fill(anglex_e,wgt_full);
-    hist_thyz_e12P->Fill(angley_e,wgt_full);
-    The2P1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {The2P1->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     The2P1MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
     if (detKinBeamRot_tar==1)
-    {hist_thxz_e22P->Fill(anglex_e,wgt_full);
-    hist_thyz_e22P->Fill(angley_e,wgt_full);
-    The2P2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
+    {The2P2->Fill(detKinBeamRot_ThEl_interaction,wgt_full);
     The2P2MCS->Fill(detKinBeamRot_ThEl_interaction,wgt_full);}
-    
-    //hist_Eout_9_e->Fill(Eout_9,wgt_full);
-    
-if (detKinBeamRot_E_clus3x3!=0) {E3x32P->Fill(detKinBeamRot_ThEl_interaction,detKinBeamRot_E_clus3x3,wgt_full);}}}
+}}
      
 }
-
 
 TH1F *Eff1 = new TH1F("ef1", "Efficency Tar1", 250,0,50);
 TH1F *Eff2 = new TH1F("ef2", "Efficency Tar2", 250,0,50);

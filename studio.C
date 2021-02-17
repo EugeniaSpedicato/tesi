cb583b075b394eecbbc982083fd21dda21af3a43
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-17 14:56:52 +0100

    png

--- studio.C
+++ studio.C
@@ -144,7 +144,7 @@ for(int i=0; i<9; ++i)
     for(int i=1;i<26;++i)
     {Etotcal+=en_c[i];}
            
-    double Eout=(Etotcal-E_clus3x3)/E_clus3x3;
+    double Eout=(Etotcal-E_clus3x3)/16;
 
 if(CentralCell==7 || CentralCell==8 || CentralCell==9 || CentralCell==12 || CentralCell==13 || CentralCell==14 || CentralCell==17 || CentralCell==18 || CentralCell==19)
 { if(r_mu<1.7 && detKinBeamRot_def_angle_mu>0.2 && E_clus3x3>1)

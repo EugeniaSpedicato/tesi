a01ceef225d3c3da8e4efbf3197febd88f242515
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-17 14:51:07 +0100

    png

--- studio.C
+++ studio.C
@@ -150,7 +150,7 @@ if(CentralCell==7 || CentralCell==8 || CentralCell==9 || CentralCell==12 || Cent
 { if(r_mu<1.7 && detKinBeamRot_def_angle_mu>0.2 && E_clus3x3>1)
         {TheCUT->Fill(detKinBeamRot_def_angle_e,wgt_full);
          if(n_cell_ph!=0)hist_E9_e->Fill(Eout,wgt_full);
-         if(n_cell_ph==0))hist_E9_eLO->Fill(Eout,wgt_full);
+         if(n_cell_ph==0)hist_E9_eLO->Fill(Eout,wgt_full);
     
         if (E_clus3x3!=0){E3x31CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);} 
         

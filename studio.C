550ab85c7f614e69883b0c92c1d10e45bed6a481
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-17 20:27:59 +0100

    png

--- studio.C
+++ studio.C
@@ -179,13 +179,12 @@ if (SeconCentralCell==Array9[i] && SeconCentralCell!=0)
 else continue;
 }
 
+if(SeconCentralCell!=0){++tot; if(n_cell_ph!=0){++sec_9;}
+                         else{++sec_9;}}     
+    
     if(E_clus3x3>1)//r_mu<1.7 && detKinBeamRot_def_angle_mu>0.2 &&
 
-    {
-if(SeconCentralCell!=0){++tot; if(n_cell_ph!=0){++sec_9;}
-                         else{++sec_9;}}
-        
-TheCUT->Fill(detKinBeamRot_def_angle_e,wgt_full);
+    {TheCUT->Fill(detKinBeamRot_def_angle_e,wgt_full);
 
 if(photon_coox!=-100 && n_cell_ph!=0)hist_E9_e->Fill(Eout,wgt_full);
 if(photon_cooy==-100)hist_E9_eLO->Fill(Eout,wgt_full);

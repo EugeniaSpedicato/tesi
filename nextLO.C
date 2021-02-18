a3c2fd686ad885d30b1ec7c7f8174988499ca9ab
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-18 12:54:49 +0100

    p

--- nextLO.C
+++ nextLO.C
@@ -103,7 +103,7 @@ TH2F  *Th_E_el2  = new TH2F("ThEel2" , " Theta el Vs. E_ECAL TAR 2",120,0,60,280
        //prende solo eventi che hanno elettroni nel calorimetro
 double r_mu=sqrt((detKinBeamRot_x_in*detKinBeamRot_x_in)+(detKinBeamRot_y_in*detKinBeamRot_y_in));   
        
-      if (r_mu<5 && E_ECAL>0.2 && abs(detKinBeamRot_cooXe) < 0.07125 && abs(detKinBeamRot_cooYe) < 0.07125 ) 
+      if (r_mu<5 && E_ECAL>1 && abs(detKinBeamRot_cooXe) < 0.07125 && abs(detKinBeamRot_cooYe) < 0.07125 ) 
 {
 
         if (abs(detKinBeamRot_cooXmu) < 0.07125 && abs(detKinBeamRot_cooYmu) < 0.07125){

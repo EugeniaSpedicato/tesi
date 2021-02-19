4942c9e90da4646af1c4e2afcd60c04562d73e8c
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-19 09:50:32 +0100

    png

--- code/FastSim.cc
+++ code/FastSim.cc
@@ -107,8 +107,9 @@ Double_t ThMuINT=b[18][3]; // angolo che ha nel momento dell'interazione, senza 
 
 detKinBeamRot.x_in=xin;
 detKinBeamRot.y_in=yin;
-
-  
+xin=xin*100;
+yin=yin*100;
+Double_t r_mu=sqrt((xin*xin)+(yin*yin));  
 //LoadKineVars(p_mu_in_div, p_e_in_div, p_mu_out_div_smeared, p_e_out_div_smeared, coo, TheINT, detKinBeamRot);
 
 //prepare for EMShower
@@ -146,7 +147,7 @@ TMatrixD cooPH=MCSphoton(p_gamma_Lab_div,xin,yin);
 double ECAL_E= energy_sm_el+en_ph_sm;   
 double cellPH=myGrid->GiveCentralCell(cooPH[0][0]*100,cooPH[0][1]*100);
 LoadPhoton(event, photon,p_gamma_Lab_div,cooPH[0][0],cooPH[0][1],myGrid);
-    if (ECAL_E>0.2)
+    if (r_mu<1.7 && ECAL_E>0.2)
     {
     //for electrons
     if(energy_sm_el>0.2 && cellEL!=0)
@@ -174,7 +175,7 @@ LoadPhoton(event, photon,p_gamma_Lab_div,cooPH[0][0],cooPH[0][1],myGrid);
  } else LoadECAL(detKinBeamRot,myGrid,i);
 
 }
-else if (energy_sm_el>0.2){
+else if (r_mu<1.7 && energy_sm_el>0.2){
 if(cellEL!=0){
 nPart=1; 
 X0depth=0.;

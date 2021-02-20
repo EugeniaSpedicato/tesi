09246da64790a89a4c884cf5cff6c85bb7b005d2
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-20 21:42:32 +0100

    png

--- studio.C
+++ studio.C
@@ -51,8 +51,8 @@ TH1F* The1CUT=new TH1F("th", "th El TAR 1 core CUT", 120,0,30);
 TH1F* The2CUT=new TH1F("th", "th El TAR 2 core CUT", 120,0,30);
 
 
-TH2F  *E3x31CUT  = new TH2F("ThEel1" , " Th_el Vs. E_E3x3 core NLO CUT",90,0,30,280,0,140);
-TH2F  *E3x32CUT  = new TH2F("ThEel2" , " Th_el Vs. E_E3x3 core LO CUT",90,0,30,280,0,140);
+TH2F  *E3x31CUT  = new TH2F("ThEel1" , " Th_el Vs. E_E3x3 core NLO CUT",150,0,30,560,0,140);
+TH2F  *E3x32CUT  = new TH2F("ThEel2" , " Th_el Vs. E_E3x3 core LO CUT",150,0,30,560,0,140);
     
     
 TH2F  *Th  = new TH2F("ThEel1" , " Th_el Vs. Th_mu  core NLO CUT",90,0,30,50,0,5);

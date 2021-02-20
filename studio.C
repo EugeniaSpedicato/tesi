f01135e4101d0a97890c3d41b1151424f695e05c
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-20 09:34:26 +0100

    png

--- studio.C
+++ studio.C
@@ -241,18 +241,19 @@ double dist=sqrt((x-detKinBeamRot_cooXe)*(x-detKinBeamRot_cooXe)+(y-detKinBeamRo
 
 if (dist<2){
     hist_E92_e->Fill(E2nd,wgt_full);
-    hist_E92_eLO->Fill(E2nd,wgt_LO);}
+    hist_E92_eLO->Fill(E2nd,wgt_LO);
+if(n_cell_ph!=0){n_cut_ph+=wgt_full;}else n_cut_noph+=wgt_full;
     
     /*if(SeconCentralCell_in9==n_cell_ph)
     {n_cut_ph+=wgt_full;} 
     else if(SeconCentralCell_in9==n_cell_e)
     {n_cut_noph+=wgt_full; }*/
-if(n_cell_ph!=0){n_cut_ph+=wgt_full;}else n_cut_noph+=wgt_full;
+
     
 hist_E9_e->Fill(E9,wgt_full);
 hist_E9_eLO->Fill(E9,wgt_LO); 
 hist_Eout_e->Fill(Eout,wgt_full);
-hist_Eout_eLO->Fill(Eout,wgt_LO); } 
+hist_Eout_eLO->Fill(Eout,wgt_LO); }} 
 else if(SeconCentralCell!=0)
 {
 hist_E9_eOUT->Fill(E9,wgt_full);

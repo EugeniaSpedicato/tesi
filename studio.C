a11c698e8ed51e7a151a517b3a967619fa1dbd86
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-20 11:35:27 +0100

    png

--- studio.C
+++ studio.C
@@ -241,22 +241,24 @@ double y = myGrid->GetYaxis()->GetBinCenter(Rev_numberY[SeconCentralCell_in9]);
 double dist=sqrt((x-detKinBeamRot_cooXe)*(x-detKinBeamRot_cooXe)+(y-detKinBeamRot_cooYe)*(y-detKinBeamRot_cooYe)); 
      
     
-if (dist>1.425 && dist<4 ){
+//if (dist>1.425 && dist<4 ){
     hist_E92_e->Fill(E2nd,wgt_full);
     hist_E92_eLO->Fill(E2nd,wgt_LO);
     
-/*hist_E9_e->Fill(E9,wgt_full);
+hist_E9_e->Fill(E9,wgt_full);
 hist_E9_eLO->Fill(E9,wgt_LO); 
 hist_Eout_e->Fill(Eout,wgt_full);
-hist_Eout_eLO->Fill(Eout,wgt_LO);*/ }} 
-else if(SeconCentralCell!=0 && E9>0.87 && Eout>0.07)
+hist_Eout_eLO->Fill(Eout,wgt_LO); //}
+} 
+
+else if(SeconCentralCell!=0)// && E9>0.87 && Eout>0.07
 {if(n_cell_ph!=0){n_cut_ph+=wgt_full;}else if(n_cell_e!=0 && n_cell_ph==0) n_cut_noph+=wgt_full; 
-/*hist_E9_eOUT->Fill(E9,wgt_full);
+hist_E9_eOUT->Fill(E9,wgt_full);
 hist_E9_eLOOUT->Fill(E9,wgt_LO); 
 hist_Eout_eOUT->Fill(Eout,wgt_full);
 hist_Eout_eLOOUT->Fill(Eout,wgt_LO);
 hist_E92_eOUT->Fill(E2nd,wgt_full);
-hist_E92_eLOOUT->Fill(E2nd,wgt_LO);*/}
+hist_E92_eLOOUT->Fill(E2nd,wgt_LO);}
     
 //else if (SeconCentralCell_in9!=0){sec_9+=wgt_full;energy->Fill(Eout,wgt_full);}
 

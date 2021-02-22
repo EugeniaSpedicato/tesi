9701f4e555b6ed32ff9248c0a5bd694aaec4415f
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-22 17:52:13 +0100

    png

--- 2studio.C
+++ 2studio.C
@@ -350,6 +350,27 @@ Th1->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
         }
     }
     
+    // ZONA 4
+    if(detKinBeamRot_def_angle_e>15 && detKinBeamRot_def_angle_e<30)// E_clus3x3<(20-(10/7)*detKinBeamRot_def_angle_e -- (E_clus3x3<5 && detKinBeamRot_def_angle_e<11)
+    {
+        if(detKinBeamRot_def_angle_mu<0.2)
+        {hist_E3x3_eCUT->Fill(E_clus3x3,wgt_full);
+    hist_E9_eCUT->Fill(E9,wgt_full);
+    hist_E92_eCUT->Fill(Emean_out,wgt_full);
+    hist_Eout_eCUT->Fill(Eout,wgt_full); 
+    hist_distCUT->Fill(ddd,wgt_full);
+    E3x32CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
+    Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
+        } else
+        {
+         hist_E3x3_e->Fill(E_clus3x3,wgt_full);
+         hist_E9_e->Fill(E9,wgt_full);
+         hist_E92_e->Fill(Emean_out,wgt_full);
+         hist_Eout_e->Fill(Eout,wgt_full); 
+         hist_dist->Fill(ddd,wgt_full);
+        }
+    }
+    
 
 
 /* 

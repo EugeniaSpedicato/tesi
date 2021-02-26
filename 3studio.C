cc5ed5ec3d8fe3f9a6674d7344f26e88a2b000d2
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-26 22:32:54 +0100

    png

--- 3studio.C
+++ 3studio.C
@@ -322,8 +322,7 @@ if(photon_energy!=-1 && n_cell_ph!=0){
 if(E_clus3x3<1){
     E3x32CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
     Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);}
-    else {E3x31CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
-Th1->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
+    else {
    
 double eq=3*sqrt((5.78/sqrt(E_clus3x3))*(5.78/sqrt(E_clus3x3))+1.095*1.095)*0.1;
 //ZONA 1

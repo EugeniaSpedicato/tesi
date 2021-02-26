cf211b6a792b2c5c91694301a5ba459a54df264e
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-26 16:30:26 +0100

    png

--- 3studio.C
+++ 3studio.C
@@ -317,7 +317,7 @@ if(photon_energy!=-1 && n_cell_ph!=0){
  diff_th_phe->Fill(diffTh,wgt_full);
  diff_r_phe->Fill(d_e_ph,wgt_full);}
 
-    if(ddd>(4*sqrt((5.78/sqrt(E_clus3x3))*(5.78/sqrt(E_clus3x3))+1.095*1.095)*0.1))
+    if(ddd>=(sqrt((5.78/sqrt(E_clus3x3))*(5.78/sqrt(E_clus3x3))+1.095*1.095)*0.1))
     { cout << (4*sqrt((5.78/sqrt(E_clus3x3))*(5.78/sqrt(E_clus3x3))+1.095*1.095)*0.1) << endl;
         DeltaRCUT->Fill(ddd,wgt_full);
         if(photon_energy!=-1 && n_cell_ph!=0){

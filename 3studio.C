3f58e41f4aff50b6c2f7ee2a94bc68b1d8bc172b
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-27 18:44:31 +0100

    png

--- 3studio.C
+++ 3studio.C
@@ -515,7 +515,7 @@ if(E_clus3x3>50 && E_clus3x3<=60){
             ThphoutCUT->Fill(photon_def_angle_ph,wgt_full);
             diff_th_pheCUT->Fill(diffTh,wgt_full);
             diff_r_pheCUT->Fill(d_e_ph,wgt_full);} 
-                    EeoutCUT->Fill(detKinBeamRot_Ee,wgt_LO); 
+                EeoutCUT->Fill(detKinBeamRot_Ee,wgt_LO); 
                TheoutCUT->Fill(detKinBeamRot_def_angle_e,wgt_LO); 
                 EmuoutCUT->Fill(detKinBeamRot_Emu,wgt_LO); 
                ThmuoutCUT->Fill(detKinBeamRot_def_angle_mu,wgt_LO); 

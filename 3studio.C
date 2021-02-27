c09e4bfc72793e4d922c94afd606fd365ecf6483
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-27 19:16:23 +0100

    png

--- 3studio.C
+++ 3studio.C
@@ -356,7 +356,7 @@ if(E_clus3x3>1){
     
 if(E_clus3x3<=10){
 
-    if(ddd>(3/4)*0.1*8.8)
+    if(ddd>(3.5/4)*0.1*8.8)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -389,7 +389,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 2
     
 if(E_clus3x3>10 && E_clus3x3<=20){
-if(ddd>(3/4)*0.1*8.5)
+if(ddd>(3.5/4)*0.1*8.5)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -418,7 +418,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 3
     
 if(E_clus3x3>20 && E_clus3x3<=30){
-if(ddd>(3/4)*0.1*7)
+if(ddd>(3.5/4)*0.1*7)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -448,7 +448,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 4
     
 if(E_clus3x3>30 && E_clus3x3<=40){
-if(ddd>(3/4)*0.1*5.85)
+if(ddd>(3.5/4)*0.1*5.85)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -477,7 +477,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 5
     
 if(E_clus3x3>40 && E_clus3x3<=50){
- if(ddd>(3/4)*0.1*5.7)
+ if(ddd>(3.5/4)*0.1*5.7)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -506,7 +506,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 6
     
 if(E_clus3x3>50 && E_clus3x3<=60){
- if(ddd>(3/4)*0.1*5.4)
+ if(ddd>(3.5/4)*0.1*5.4)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -536,7 +536,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 7
     
 if(E_clus3x3>60 && E_clus3x3<=75){
-  if(ddd>(3/4)*0.1*5.12)
+  if(ddd>(3.5/4)*0.1*5.12)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);
@@ -565,7 +565,7 @@ Th2->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
 //ZONA 8
     
 if(E_clus3x3>75){
-  if(ddd>(3/4)*0.1*4.9)
+  if(ddd>(3.5/4)*0.1*4.9)
     {
  
         DeltaRCUT->Fill(ddd,wgt_full);

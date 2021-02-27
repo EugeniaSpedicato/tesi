f5e2f07ba8e2c001b3987b2bd635ad79d8f5cdea
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-27 10:26:06 +0100

    png

--- 2studio.C
+++ 2studio.C
@@ -382,7 +382,7 @@ E3x32CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
     
 
     // ZONA 3
-    if(detKinBeamRot_def_angle_e>10 && detKinBeamRot_def_angle_e<=13.5)// E_clus3x3<(20-(10/7)*detKinBeamRot_def_angle_e -- (E_clus3x3<5 && detKinBeamRot_def_angle_e<11)
+    if(detKinBeamRot_def_angle_e>10 && detKinBeamRot_def_angle_e<=13)// E_clus3x3<(20-(10/7)*detKinBeamRot_def_angle_e -- (E_clus3x3<5 && detKinBeamRot_def_angle_e<11)
     {
         if(detKinBeamRot_def_angle_mu<0.3)
         { /*hist_E3x3_eCUT->Fill(E_clus3x3,wgt_full);
@@ -414,7 +414,7 @@ E3x32CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
     
     
     // ZONA 4
-    if(detKinBeamRot_def_angle_e>13.5 && detKinBeamRot_def_angle_e<=18.5)// E_clus3x3<(20-(10/7)*detKinBeamRot_def_angle_e -- (E_clus3x3<5 && detKinBeamRot_def_angle_e<11)
+    if(detKinBeamRot_def_angle_e>13 && detKinBeamRot_def_angle_e<=18.5)// E_clus3x3<(20-(10/7)*detKinBeamRot_def_angle_e -- (E_clus3x3<5 && detKinBeamRot_def_angle_e<11)
     {
         if(detKinBeamRot_def_angle_mu<0.2)
         { /*hist_E3x3_eCUT->Fill(E_clus3x3,wgt_full);

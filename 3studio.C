b8de385d8d3afa91063e5f8f53029544c2c3e303
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-02-27 10:44:30 +0100

    png

--- 3studio.C
+++ 3studio.C
@@ -325,11 +325,20 @@ if(photon_energy!=-1 && n_cell_ph!=0){
  diff_r_phe->Fill(d_e_ph,wgt_full);}
     
 
-//if(E_clus3x3>1){
+if(E_clus3x3>1){
+    
+    E3x31CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
+Th1->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
+
+    if(photon_energy!=-1 && n_cell_ph!=0){
+ EphoutCUTafter->Fill(photon_energy,wgt_full);
+ ThphoutCUTafter->Fill(photon_def_angle_ph,wgt_full);
+ diff_th_pheCUTafter->Fill(diffTh,wgt_full);
+ diff_r_pheCUTafter->Fill(d_e_ph,wgt_full);}
 
 //ZONA 1
     
-if(E_clus3x3<=10){
+/*if(E_clus3x3<=10){
 
     if(ddd>0.1*8.8)
     {
@@ -531,8 +540,8 @@ Th1->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);
  diff_th_pheCUTafter->Fill(diffTh,wgt_full);
  diff_r_pheCUTafter->Fill(d_e_ph,wgt_full);}
 } 
+}*/
 }
-/*}
 else {
         if(photon_energy!=-1 && n_cell_ph!=0){
             EphoutCUT->Fill(photon_energy,wgt_full);

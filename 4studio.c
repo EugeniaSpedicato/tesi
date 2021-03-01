6730194147c6fdffc0f824509a5ba3a48b623ea4
Eugenia Spedicato <eugenia.spedicato@studio.unibo.it>
2021-03-01 15:37:43 +0100

    png

--- 4studio.c
+++ 4studio.c
@@ -64,7 +64,7 @@ TH1F* hist_E3x3_e=new TH1F("E3x3", "Energy Reco 3x3", 70,0.,140);
 TH1F* hist_E3x3_eCUT=new TH1F("E3x3cut", "Energy Reco 3x3 cut", 70,0.,140); 
 
 
-TH1F* hist_DE=new TH1F("E3x3", "DE", 100,-2,2);
+TH1F* hist_DE=new TH1F("E3x3", "DE", 100,0,2);
 TH1F* hist_DE5=new TH1F("E3x3", "DE", 100,-0.01,0.01);
     
     
@@ -328,7 +328,7 @@ double beta=(sqrt(150*150-(m_mu*m_mu))/(150+m_e));
 double Ethe=m_e*((1+(beta*beta*cos(x)*cos(x)))/(1-(beta*beta*cos(x)*cos(x))));
 //cout << " E teorico " << Ethe  << " E reco " << E_clus3x3 << endl;
 double der_Ee=-4*m_e*beta*beta*( (cos(x)*sin(x))/((1-(beta*beta*cos(x)*cos(x)))*(1-(beta*beta*cos(x)*cos(x)))) );
-double DE= (E_clus3x3-(95.4/100)*Ethe)/100;
+double DE= (E_clus3x3/(95.4/100)*Ethe);
 double DE5= (E_clus3x3-(95.4/100)*Ethe)/(sqrt(der_Ee*der_Ee+1));
 // /(sqrt(der_Ee*der_Ee+1));
 // cout << " DE " << DE << " E_clus3x3-Ethe " << E_clus3x3-Ethe << endl;
@@ -342,7 +342,7 @@ if(r_mu<1.7 && detKinBeamRot_tar==1 ){
 if (detKinBeamRot_def_angle_e<10) hist_DE->Fill(DE,wgt_full);   
 if (detKinBeamRot_def_angle_e>10) hist_DE5->Fill(DE5,wgt_full);   
        
-if (DE>-0.9 && DE<0.9 && detKinBeamRot_def_angle_e<10) 
+if (DE>1 && DE<0.8 && detKinBeamRot_def_angle_e<10) 
 {E3x31CUT->Fill(detKinBeamRot_def_angle_e,E_clus3x3,wgt_full);
 Th1->Fill(detKinBeamRot_def_angle_e,detKinBeamRot_def_angle_mu,wgt_full);}   
 if (DE5>-0.006 && detKinBeamRot_def_angle_e>10) 
